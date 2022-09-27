857e3f492d6f750a1983be16bcde1bff805034b9
Paul de Witt <pdewitt@gmail.com>
2022-09-27 13:38:37 +0200

    added-shadowboxing

--- cypress/component/MyElement.cy.ts
+++ cypress/component/MyElement.cy.ts
@@ -2,19 +2,19 @@ import { MyElement } from '../../src/my-element';
 
 describe('MyElement.cy.ts', () => {
   it('should validate its functionality', () => {
-    cy.get('button').should('contain.text', 'Click Count: 0');
-    cy.get('button').click();
-    cy.get('button').should('contain.text', 'Click Count: 1');
+    cy.get('my-element').shadow('button').should('contain.text', 'Click Count: 0');
+    cy.get('my-element').shadow().find('button').click();
+    cy.get('my-element').shadow('button').should('contain.text', 'Click Count: 1');
   });
 
   it('should validate the styling for the H1 element', () => {
-    cy.get('h1').should('contain.text', 'Hello, World');
-    cy.get('h1').should('have.css', 'color', 'rgb(0, 128, 0)');
+    cy.get('my-element').shadow('h1').should('contain.text', 'Hello, World');
+    cy.get('my-element').shadow().find('h1').should('have.css', 'color', 'rgb(0, 128, 0)');
   });
   
   it('should validate the styling for the button element', () => {
-    cy.get('button').should('contain.text', 'Click Count:');
-    cy.get('button').should('have.css', 'background-color', 'rgb(255, 255, 0)');
+    cy.get('my-element').shadow('button').should('contain.text', 'Click Count:');
+    cy.get('my-element').shadow().get('button').should('have.css', 'background-color', 'rgb(255, 255, 0)');
   });
 
 before(() => {
